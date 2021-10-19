21e28d4cab84eabf0435b5e2c163ce29d1aba8fa
shenjiafei <295417878@qq.com>
2021-10-19 15:43:28 +0800

    first commit

--- Example/DeviceHelper/JFViewController.m
+++ Example/DeviceHelper/JFViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"我是 podlib 测试项目");
 }
 
 - (void)didReceiveMemoryWarning
